9ddfc3e6ffc8b2fab332c51b1457bd9ac1450434
Robin Tweedie <robin@songkick.com>
2012-05-24 17:01:03 +0100

    adding bash_aliases to install script
    and moving out of a folder.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+cp bash_aliases $HOME/.bash_aliases
 cp gemrc $HOME/.gemrc
-cp screenrc $HOME/.screenrc
-cp vimrc    $HOME/.vimrc
 cp gitconfig $HOME/.gitconfig
 cp gitignore $HOME/.gitignore
+cp screenrc $HOME/.screenrc
+cp vimrc    $HOME/.vimrc
 
 echo "Your config has been copied to $HOME"
