3ad56235690b9a4ff92d6604d43b5b155dc2d7f8
Robin Tweedie <robin@songkick.com>
2012-11-15 11:29:41 +0000

    make derp for vimswappage

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 cp bash_aliases $HOME/.bash_aliases
-cp gemrc $HOME/.gemrc
-cp gitconfig $HOME/.gitconfig
-cp gitignore $HOME/.gitignore
-cp screenrc $HOME/.screenrc
-cp vimrc    $HOME/.vimrc
+cp gemrc        $HOME/.gemrc
+cp gitconfig    $HOME/.gitconfig
+cp gitignore    $HOME/.gitignore
+cp screenrc     $HOME/.screenrc
+cp vimrc        $HOME/.vimrc
+mkdir -p        $HOME/.vimswap
 
 echo "Your config has been copied to $HOME"
 exec bash -l
